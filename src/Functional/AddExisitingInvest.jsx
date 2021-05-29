5c2d61515e18c3ece045111fef0de24897fa364b
Raj Shah <rshah62@uncc.edu>
2021-05-29 01:47:02 -0400

    ⚡️ : Codebase Updated

--- src/Functional/AddExisitingInvest.jsx
+++ src/Functional/AddExisitingInvest.jsx
@@ -1,37 +1,34 @@
 import React, { useState } from 'react';
-import { auth, firestore } from '../componets/Firebase';
+import { auth } from '../componets/Firebase';
 import '../stylefiles/addExistingInevst.css';
 import axios from 'axios';
 
 function AddExistingInvest() {
     var [familycode, SetFamilyCode] = useState("");
-    var [currentUserId, SetcurrentUserId] = useState(auth.currentUser.uid);
-    var [investObject, SetInvestObject] = useState(null);
+    var currentUserId = auth.currentUser.uid;
     var [investorInfoList, SetInvestorInfoList] = useState([]);
 
-    
-
-    
-
-    const SearchDBByfamilyCode = async(e) => {
-        var responseData= await axios.get(`https://fdproject-api.azurewebsites.net/${currentUserId}/getInvestorsFromID/${familycode}`).then(
-            res=>{
-                SetInvestorInfoList((perv)=>[...perv,...res.data]);
+    const SearchDBByfamilyCode = async (e) => {
+        // If SomeOne access this Button Once Again Clear Last values
+        SetInvestorInfoList([]);
+         await axios.get(`https://fdproject-api.azurewebsites.net/${currentUserId}/getInvestorsFromID/${familycode}`).then(
+            res => {
+                SetInvestorInfoList((perv) => [...perv, ...res.data]);
             }
         );
-        //console.log(responseData.data);
-        //SetInvestorInfoList((perv)=>[...perv,...responseData.data]);
     };
     return (
         <div className="addExist-hero">
             <input type='text' className='familycodeSearch' value={familycode} onChange={e => SetFamilyCode(e.target.value)} ></input>
             <button className="three-view-button" onClick={e => SearchDBByfamilyCode(e)}>Search</button>
-            
-            <div>{investorInfoList.map(investor=>
-                <div key={investor.id}>{investor.firstName}</div>
-            )}</div>
+            <br/><br/><br/>
+            <div className="display-result-table">
 
-            
+                <div>{investorInfoList.map(investor =>
+                    <div key={investor.id}>{investor.firstName}</div>
+                )}</div>
+            </div>
+
         </div>
     );
 }
